5683e7997f2ce6897bc788ff5103b83d7dc55f68
sentissi1 <sentissielmehdi@gmail.com>
2023-12-04 09:48:56 -0800

    Python

--- 0x03-python-data_structures/lists.h
+++ 0x03-python-data_structures/lists.h
@@ -1,4 +1,3 @@
-#include <stddef.h>
 #ifndef LISTS_H
 #define LISTS_H
 
@@ -12,8 +11,8 @@
  */
 typedef struct listint_s
 {
-    int n;
-    struct listint_s *next;
+	int n;
+	struct listint_s *next;
 } listint_t;
 
 size_t print_listint(const listint_t *h);
